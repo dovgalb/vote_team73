e6f968800dcb6df75054cb06aae26db56a189062
Shcherbatov Bogdan <shcherbatov@firecode-team.ru>
2023-11-22 13:15:04 +0300

    postgres, nginx

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+FROM python:3.11.2 as requirements-stage
+
+#
+WORKDIR /tmp
+
+#
+RUN pip install poetry
+
+#
+COPY ./pyproject.toml ./poetry.lock* /tmp/
+
+#
+RUN poetry export -f requirements.txt --output requirements.txt --without-hashes
+
+#
+FROM python:3.11.2
+
+#
+WORKDIR /code
+
+#
+COPY --from=requirements-stage /tmp/requirements.txt /code/requirements.txt
+
+#
+RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
+
+#
+COPY entrypoint.sh /tmp/entrypoint.sh
+
+COPY . .
+
+ENTRYPOINT ["sh", "/tmp/entrypoint.sh"]
--- project/settings.py
+++ project/settings.py
@@ -11,22 +11,26 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k^#20a((um=8nl-g1we^(l9d!6o6s@qi@da&jopx3a5nzojjo#'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(" ")
 
 if DEBUG:
     import socket  # only if you haven't already imported this
@@ -95,13 +99,24 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('POSTGRES_ENGINE'),
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
