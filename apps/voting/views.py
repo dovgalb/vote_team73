43db01df3bc4bf212ebf00c35c92bf4d45bb9693
Shcherbatov Bogdan <shcherbatov@firecode-team.ru>
2023-11-27 18:39:10 +0300

    DTO

--- apps/voting/views.py
+++ apps/voting/views.py
@@ -6,7 +6,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
 
-from .models import Voting
+from .models import Voting, VotingCharacter
 from .serializers import VotingSerializer, MakeVotingSerializer
 from .serializers import CharacterSerializer
 from .services import make_voting
@@ -18,9 +18,33 @@ class CharacterViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = CharacterSerializer
 
 
-class VotingViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
-    queryset = Voting.objects.all()
+class VotingViewSet(mixins.ListModelMixin, viewsets.GenericViewSet, mixins.RetrieveModelMixin):
+    queryset = Voting.objects.all().prefetch_related('characters')
     serializer_class = VotingSerializer
+
+    @action(detail=False, methods=['get'])
+    def active_votings(self, request):
+        """
+        Представление для просмотра АКТИВНЫХ голосований
+        """
+        active_votings = self.get_queryset().filter(is_active=True)
+        serializer = self.get_serializer(active_votings, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['get'])
+    def inactive_votings(self, request):
+        """
+        Представление для просмотра НЕАКТИВНЫХ голосований
+        """
+        inactive_votings = self.get_queryset().filter(is_active=False)
+        serializer = self.get_serializer(inactive_votings, many=True)
+        return Response(serializer.data)
+
+    # @action(detail=False, methods=['get'])
+    # def get_winners(self, request):
+    #     winners = VotingCharacter.objects.filter(voting__is_active=False)
+    #     serializer = self.get_serializer(winners, many=True)
+    #     return Response(serializer.data)
 
 
 class MakeVoteViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
@@ -34,4 +58,6 @@ class MakeVoteViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
         except ValidationError as exception:
             return Response({'message': exception}, status=status.HTTP_400_BAD_REQUEST)
         return Response({'message': 'Ваш голос учтен'}, status=status.HTTP_201_CREATED)
+
+
 
